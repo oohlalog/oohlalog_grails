04990969462fa6d107b0912b04e0f5b3182cc3ca
David Estes <davydotcom@gmail.com>
2013-06-26 08:34:29 -0400

    initial commit

---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,51 @@
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir = "target/test-reports"
+grails.project.repos.bertramlabsSnap.url = "http://nexus.bertramlabs.com/content/repositories/snapshots"
+grails.project.repos.bertramlabsSnap.type = "maven"
+grails.project.repos.bertramlabsSnap.username = System.getProperty('project.repos.bertramlabsSnap.username')
+grails.project.repos.bertramlabsSnap.password = System.getProperty('project.repos.bertramlabsSnap.password')
+
+grails.project.repos.bertramlabsRel.url = "http://nexus.bertramlabs.com/content/repositories/releases"
+grails.project.repos.bertramlabsRel.type = "maven"
+grails.project.repos.bertramlabsRel.username = System.getProperty('project.repos.bertramlabsRel.username')
+grails.project.repos.bertramlabsRel.password = System.getProperty('project.repos.bertramlabsRel.password')
+
+
+grails.project.repos.default = "bertramlabsSnap"
+grails.release.scm.enabled = false
+
+
+grails.project.dependency.resolution = {
+    // inherit Grails' default dependencies
+    inherits("global") {
+        // uncomment to disable ehcache
+        // excludes 'ehcache'
+    }
+    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    legacyResolve true // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
+    repositories {
+        grailsCentral()
+        // uncomment the below to enable remote dependency resolution
+        // from public Maven repositories
+        //mavenLocal()
+        //mavenCentral()
+        //mavenRepo "http://snapshots.repository.codehaus.org"
+        //mavenRepo "http://repository.codehaus.org"
+        //mavenRepo "http://download.java.net/maven/2/"
+        //mavenRepo "http://repository.jboss.com/maven2/"
+    }
+    dependencies {
+        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+
+        // runtime 'mysql:mysql-connector-java:5.1.21'
+    }
+
+    plugins {
+        build(":tomcat:$grailsVersion",
+              ":release:2.2.0",
+              ":rest-client-builder:1.0.3") {
+            export = false
+        }
+    }
+}
