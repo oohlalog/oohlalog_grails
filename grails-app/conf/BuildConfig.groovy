b939a5e805d6320c8328fb3fac50de4fdc992bd8
David Estes <davydotcom@gmail.com>
2014-03-11 09:41:17 -0400

    switching log4j dependency to bertram labs nexus repo

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -6,14 +6,17 @@ grails.project.dependency.resolution = {
 
     repositories {
         grailsCentral()
+        grailsPlugins()
         mavenLocal()
         mavenCentral()
+        mavenRepo "http://nexus.bertramlabs.com/content/repositories/publicReleases"
     }
 
     dependencies {
+        compile 'oohlalog:oohlalog-4j:0.1.1'
 //        runtime 'com.google.code.gson:gson:2.2.2'
 //        compile 'com.google.code.gson:gson:2.2.2'
-//        build 'com.google.code.gson:gson:2.2.2'
+       // build 'com.google.code.gson:gson:2.2.2'
     }
 
     plugins {
