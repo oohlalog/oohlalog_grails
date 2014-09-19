00bbfb520518b57c787c635c256cefc706d2b305
David Estes <davydotcom@gmail.com>
2014-09-19 15:16:12 -0400

    fixing bug in initialization of oohlalog

--- OohLaLogGrailsPlugin.groovy
+++ OohLaLogGrailsPlugin.groovy
@@ -7,7 +7,7 @@ import com.oohlalog.grails.OohLaLogFilters
 import com.oohlalog.grails.OohLaLogAuthenticationFilter
 
 class OohLaLogGrailsPlugin {
-	def version         = "0.4.4"
+	def version         = "0.4.5"
 	def grailsVersion   = "2.0 > *"
 	def title           = "OohLaLog Plugin" // Headline display name of the plugin
 	def organization    = [ name: "OohLaLog", url: "http://www.oohlalog.com/" ]
@@ -104,7 +104,7 @@ class OohLaLogGrailsPlugin {
         }
 //        println OohLaLogFilters.CONFIGURED_CONTROLLER_NAMES
         try {
-            OohLaLogAppender appender = Logger.rootLogger.getAllAppenders().each {
+            Logger.rootLogger.getAllAppenders().each {
                 if(it instanceof OohLaLogAppender) {
 					if(it.agent == 'log4j') {
 						it.agent = 'grails:'+version
